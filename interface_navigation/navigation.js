46bf018d8fbc185c5f86e8f741d70703577ec450
ilyasbougdour <ilyasbougdour6@gmail.com>
2025-01-08 02:19:54 +0100

    updates

--- interface_navigation/navigation.js
+++ interface_navigation/navigation.js
@@ -1,15 +1,6 @@
 const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 
-allSideMenu.forEach(item => {
-    const li = item.parentElement;
 
-    item.addEventListener('click', function () {
-        allSideMenu.forEach(i => {
-            i.parentElement.classList.remove('active');
-        });
-        li.classList.add('active');
-    });
-});
 
 // TOGGLE SIDEBAR
 const menuBar = document.querySelector('#content nav .bx.bx-menu');
