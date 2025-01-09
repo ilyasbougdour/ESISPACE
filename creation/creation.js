eecd61e99b0295c1f13c9639248eb72ce66c26bc
apverd <salaheddinegaam@gmail.com>
2025-01-09 23:09:46 +0100

    creation

---
+++ creation/creation.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const saveDraftBtn = document.querySelector(".save-draft");
+    const publishBtn = document.querySelector(".publish");
+
+    saveDraftBtn.addEventListener("click", () => {
+        alert("Votre brouillon a été sauvegardé !");
+    });
+
+    publishBtn.addEventListener("click", () => {
+        alert("Votre post a été publié !");
+    });
+});
